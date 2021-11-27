7f434e39950c581aff6f2664223ae04e984a1f92
vietnguyen2x <viet.nd@beetechsoft.vn>
2021-11-27 21:07:19 +0700

    login

---
+++ src/BE/middleware/client/Auth.js
@@ -0,0 +1,54 @@
+module.exports =(req, res, next) => {
+    if (req.session.customer) {
+        const pathLogin = req._parsedOriginalUrl.pathname;
+        if ((pathLogin === '/login')) {
+            res.redirect('/');
+        }
+
+        res.locals.customer = req.session.customer;
+        next();
+    } else {
+        res.locals.customer = false;
+        next();
+    }
+}
+
+// module.exports.checkValidateFormLogin = (req, res, next) =>{
+//     let errors ={err: false};
+//     if (!req.body.tenDangNhap) {
+//         errors.tenDangNhap= 'Tên đăng nhập không được để trống!!!';
+//         errors.err = true;
+//     }
+//     if (!req.body.matKhau) {
+//         errors.matKhau = 'Mật khẩu không được để trống!!!';
+//         errors.err = true;
+//     }
+//     if(errors.err){
+        
+//         res.json({
+//             errors:errors,
+//         });
+//         return;
+//     }
+//     next();
+    
+// }
+
+// module.exports.checkAccountLogin = (req,res, next) =>{
+//     User.findOne({tenDangNhap: req.body.tenDangNhap})
+//     .then(user =>{
+//         let errors ={err: false};
+//         if (!user || user.matKhau !== md5(req.body.matKhau)) {
+//             errors.existUser = 'Tên đăng nhập hoặc mật khẩu không đúng!!!';
+//             errors.err = true;
+//         }
+//         if (errors.err) {
+//             res.json({errors: errors});
+//             return;
+//         }
+//         next();
+//     })
+//     .catch(error =>{
+//         res.json({errors: error});
+//     })
+// }
