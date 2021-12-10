c1507f6612752c8640280a95b942f9ae58e4630d
StarEdit <phuongtan9920@gmail.com>
2021-12-10 17:29:27 +0700

    fix front

--- src/BE/public/js/index.js
+++ src/BE/public/js/index.js
@@ -1,5 +1,6 @@
 var account = document.querySelector(".account-list");
-document.querySelector(".account-btn").onclick = () => {
+document.querySelector(".account-btn").onclick = (e) => {
+    e.preventDefault();
     account.classList.toggle("active");
     searchForm.classList.remove("active");
 };
@@ -8,7 +9,7 @@ document.querySelector("#search-btn").onclick = () => {
     searchForm.classList.toggle("active");
     account.classList.remove("active");
 };
-window.onscroll = () => {
-    searchForm.classList.remove("active");
-    account.classList.remove("active");
-};
+// window.onscroll = () => {
+//     searchForm.classList.remove("active");
+//     account.classList.remove("active");
+// };
